d7799601ff6b347e22b4638dae7da6222d628c2b
TJ Slezak <tjslezak@hotmail.com>
2019-01-08 00:38:15 -0700

    Cleaned up messy code

--- geosemantic_segmentation_experiment.py
+++ geosemantic_segmentation_experiment.py
@@ -1,6 +1,6 @@
 import os
 import rastervision as rv
-
+from geotoolkit import build_class_color_dict
 
 def build_scene(task, data_uri, id, channel_order=None):
     ## id = id.replace('-', '_')
@@ -66,12 +66,13 @@ class GeoSemanticSegmentation(rv.ExperimentSet):
         channel_order = [0, 1, 2]
 
         debug = False
-        batch_size = 8
-        s = 5490
-        chips_per_scene = 100
-        chip_size = 549 
+        batch_size = 12
+        chips_per_scene = 225
+        chip_size = 366 
         num_steps = 150000
         model_type = rv.XCEPTION_65
+        task_type = rv.SEMANTIC_SEGMENTATION
+        ac_key = '{}_{}'.format('AZGEO', task_type.lower())
 
         if test_run:
             debug = True
@@ -81,30 +82,7 @@ class GeoSemanticSegmentation(rv.ExperimentSet):
             train_ids = train_ids[0:1]
             val_ids = val_ids[0:1]
 
-        classes = {'Q': (0, 'rgb(244,242,230)'), 'QTb': (1, 'rgb(240,213,240)'),
-                    'QTv': (2, 'rgb(227,196,240)'), 'Qr': (3, 'rgb(255,255,186)'),
-                     'Qy': (4, 'rgb(255,255,227)'), 'Qo': (5, 'rgb(253,252,155)'),
-                     'QTs': (6, 'rgb(255,232,186)'), 'Tvy': (7, 'rgb(255,168,255)'),
-                     'Tsy': (8, 'rgb(255,232,196)'), 'Tby': (9, 'rgb(255,186,227)'),
-                     'Tb': (10, 'rgb(255,158,227)'), 'Tsv': (11, 'rgb(251,196,123)'),
-                     'Tsm': (12, 'rgb(226,184,84)'), 'Tv': (13, 'rgb(253,193,88)'),
-                     'Tg': (14, 'rgb(251,186,123)'), 'Ti': (15, 'rgb(253,158,105)'),
-                     'TXgn': (16, 'rgb(251,154,170)'), 'Tso': (17, 'rgb(238,215,146)'),
-                     'TKgm': (18, 'rgb(255,135,145)'), 'TKg': (19, 'rgb(253,119,101)'),
-                     'Kv': (20, 'rgb(209,255,135)'), 'KJo': (21, 'rgb(186,255,158)'),
-                     'Kmv': (22, 'rgb(232,255,168)'), 'Ks': (23, 'rgb(179,251,133)'),
-                     'Jm': (24, 'rgb(120,240,119)'), 'Yg': (25, 'rgb(202,139,79)'),
-                     'Jsv': (26, 'rgb(125,255,178)'), 'Jg': (27, 'rgb(228,95,176)'),
-                     'J^': (28, 'rgb(178,255,240)'), '}|': (29, 'rgb(129,253,128)'),
-                     'Js': (30, 'rgb(121,226,157)'), 'Jgc': (31, 'rgb(109,241,168)'),
-                     '^c': (32, 'rgb(125,255,209)'), '^m': (33, 'rgb(144,222,212)'),
-                     '|': (34, 'rgb(123,236,251)'), 'P': (35, 'rgb(168,240,255)'),
-                     'P*': (36, 'rgb(158,217,255)'), 'M_': (37, 'rgb(196,217,240)'),
-                     'Ys': (38, 'rgb(186,178,255)'), 'Yd': (39, 'rgb(213,151,208)'),
-                     'YXg': (40, 'rgb(209,161,119)'), 'Xg': (41, 'rgb(186,130,123)'),
-                     'Xms': (42, 'rgb(185,196,185)'), 'Xq': (43, 'rgb(146,179,95)'),
-                     'Xmv': (44, 'rgb(144,179,136)'), 'Xm': (45, 'rgb(178,178,95)'),
-                     'NODATA': (46, 'rgb(0,0,0)')}
+        classes = build_class_color_dict()
 
         task = rv.TaskConfig.builder(rv.SEMANTIC_SEGMENTATION) \
                             .with_chip_size(chip_size) \
@@ -136,12 +114,14 @@ class GeoSemanticSegmentation(rv.ExperimentSet):
 
 
         experiment = rv.ExperimentConfig.builder() \
-                                        .with_id('geoss-XCEPTION-549') \
+                                        .with_id('geoss-xception-batch12') \
                                         .with_task(task) \
                                         .with_backend(backend) \
                                         .with_dataset(dataset) \
                                         .with_root_uri(root_uri) \
                                         .with_stats_analyzer() \
+                                        .with_analyze_key(ac_key) \
+                                        .with_chip_key(ac_key)
                                         .build()
 
         return experiment
